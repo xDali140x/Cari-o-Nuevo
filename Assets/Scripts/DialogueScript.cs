f897bb3118735c3773715f949848292a2defd900
CristianPena23 <est.cristian.pena1@unimilitar.edu.co>
2026-06-04 02:14:46 -0500

    DialogeNPC

--- Assets/Scripts/DialogueScript.cs
+++ Assets/Scripts/DialogueScript.cs
@@ -5,41 +5,64 @@ using TMPro;
 
 public class DialogueScript : MonoBehaviour
 {
+    [Header("UI")]
     public TMP_Text dialogueText;
+    public GameObject dialoguePanel;
+
+    [Header("Dialogue")]
     public string[] dialogueLines;
     public float typingSpeed = 0.05f;
-    int currentLineIndex = 0;
-    InputDevice rightHandDevice;
-    bool previousPrimaryButtonState = false;
-    
+
+    private InputDevice leftHandDevice;
+    private bool previousPrimaryButtonState = false;
+    private bool isPlayerInRange = false;
+    private bool isTyping = false;
+    private bool dialogueStarted = false;
+    private int currentLineIndex = 0;
+
     void Start()
     {
+        dialoguePanel.SetActive(false);
         dialogueText.text = string.Empty;
-        GetRightHandDevice();
-        StartDialogue();
+        GetLeftHandDevice();
     }
 
-    
     void Update()
     {
-        GetRightHandDevice();
+        if (isPlayerInRange && !dialogueStarted)
+        {
+            ShowDialogue();
+        }
 
+        GetLeftHandDevice();
+        HandleButtonInput();
+    }
+
+    void GetLeftHandDevice()
+    {
+        if (!leftHandDevice.isValid)
+            leftHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+    }
+
+    void HandleButtonInput()
+    {
         bool primaryButtonPressed = false;
-        if (rightHandDevice.isValid && rightHandDevice.TryGetFeatureValue(CommonUsages.primaryButton, out primaryButtonPressed))
+
+        if (leftHandDevice.isValid &&
+            leftHandDevice.TryGetFeatureValue(
+                CommonUsages.primaryButton, out primaryButtonPressed))
         {
             if (primaryButtonPressed && !previousPrimaryButtonState)
             {
-                if (dialogueText.text == dialogueLines[currentLineIndex])
-                {
+                if (!isTyping)
                     NextLine();
-                }
                 else
                 {
                     StopAllCoroutines();
                     dialogueText.text = dialogueLines[currentLineIndex];
+                    isTyping = false;
                 }
             }
-
             previousPrimaryButtonState = primaryButtonPressed;
         }
         else
@@ -48,29 +71,27 @@ public class DialogueScript : MonoBehaviour
         }
     }
 
-    void GetRightHandDevice()
+    public void ShowDialogue()
     {
-        if (!rightHandDevice.isValid)
-        {
-            rightHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
-        }
-    }
-
-    public void StartDialogue()
-    {
+        dialogueStarted = true;
         currentLineIndex = 0;
+        dialoguePanel.SetActive(true);
         dialogueText.text = "";
         StartCoroutine(TypeLine());
     }
 
     IEnumerator TypeLine()
     {
+        isTyping = true;
+        dialogueText.text = "";
         foreach (char letter in dialogueLines[currentLineIndex].ToCharArray())
         {
             dialogueText.text += letter;
             yield return new WaitForSeconds(typingSpeed);
         }
+        isTyping = false;
     }
+
     public void NextLine()
     {
         if (currentLineIndex < dialogueLines.Length - 1)
@@ -81,9 +102,30 @@ public class DialogueScript : MonoBehaviour
         }
         else
         {
-            gameObject.SetActive(false);
+            dialoguePanel.SetActive(false);
+            dialogueStarted = false;
+            currentLineIndex = 0;
         }
     }
 
-    
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+            Debug.Log("Player entered dialogue range.");
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+            dialogueStarted = false;
+            dialoguePanel.SetActive(false);
+            StopAllCoroutines();
+            isTyping = false;
+        }
+    }
 }
